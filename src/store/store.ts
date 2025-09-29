7149db18682879d6d829c89d53e1e15ebd96a435
premsla <premalatha.p2022ai-ds@sece.ac.in>
2025-09-29 22:24:53 +0530

    Initialize project using Create React App

---
+++ src/store/store.ts
@@ -0,0 +1,28 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import interviewSlice from './features/interviewSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, interviewSlice);
+
+export const store = configureStore({
+  reducer: {
+    interview: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
